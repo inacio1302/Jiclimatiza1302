76a4e3f3e859bbabc8ba4c9049e45fc7e8fe79ae
inacio1302 <inaciorodrigues.je@gmail.com>
2026-08-31 17:53:01 -0300

    Remove Google tag script from __root.tsx
    Removed Google tag (gtag.js) script from __root.tsx

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -133,9 +133,7 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
 }),
     
     
-    <!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-16ZSBVQRS1"></script>
-<script>
+  
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
