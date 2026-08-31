069e354a954b9ac72ae8b568b35f4d58e9834354
inacio1302 <inaciorodrigues.je@gmail.com>
2026-08-31 17:24:08 -0300

    Simplify root rendering logic in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from '@tanstack/react-router'
 import { router } from './router'
+import './index.css'
 
-const rootElement = document.getElementById('root')!
-if (!rootElement.innerHTML) {
-  const root = ReactDOM.createRoot(rootElement)
-  root.render(
-    <React.StrictMode>
-      <RouterProvider router={router} />
-    </React.StrictMode>,
-  )
-}
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
