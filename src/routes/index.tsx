8369d82e2914e2f4103f0054dbbc2abfecb7fd58
inacio1302 <inaciorodrigues.je@gmail.com>
2026-08-31 17:53:56 -0300

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -90,9 +90,7 @@ export const Route = createFileRoute("/")({
   ],
 }),
     
-    <!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-16ZSBVQRS1"></script>
-<script>
+    
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
