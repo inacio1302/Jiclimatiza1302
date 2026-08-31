8f34f128632ffacbf2cbdc8063c601e917dc0fb8
inacio1302 <inaciorodrigues.je@gmail.com>
2026-08-31 17:26:09 -0300

    Remove index.css import from main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from '@tanstack/react-router'
 import { router } from './router'
-import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
