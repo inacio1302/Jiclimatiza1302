2a809ebe118bf1fcda87f0bd809247f5bdef0e75
inacio1302 <inaciorodrigues.je@gmail.com>
2026-08-31 17:37:44 -0300

    Import styles.css in main.tsx
    Import correct styles.css file for the project.

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from '@tanstack/react-router'
 import { router } from './router'
+import './styles.css' // Importa o arquivo de estilos correto do seu projeto
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
